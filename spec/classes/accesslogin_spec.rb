e0728ee8f9e239c4ab3812d94e5e51a733cd07af
Garrett Honeycutt <code@garretthoneycutt.com>
2013-05-31 15:10:16 +0200

    Add spec tests

--- spec/classes/accesslogin_spec.rb
+++ spec/classes/accesslogin_spec.rb
@@ -1,23 +1,23 @@
 require 'spec_helper'
 describe 'pam::accesslogin' do
-	describe 'access.conf' do
-		context 'with default values' do
-			let(:facts) do
-				{
-					:osfamily          => 'RedHat',
-					:lsbmajdistrelease => '5',
-				}
-			end
-			it do
-				should include_class('pam')
-				should contain_file('access_conf').with({
-					'ensure' => 'file',
-					'path'   => '/etc/security/access.conf',
-					'owner'  => 'root',
-					'group'  => 'root',
-					'mode'   => '0644',
-					})
-				should contain_file('access_conf').with_content(
+  describe 'access.conf' do
+    context 'with default values on supported platform' do
+      let(:facts) do
+        {
+          :osfamily          => 'RedHat',
+          :lsbmajdistrelease => '5',
+        }
+      end
+      it do
+        should include_class('pam')
+        should contain_file('access_conf').with({
+          'ensure' => 'file',
+          'path'   => '/etc/security/access.conf',
+          'owner'  => 'root',
+          'group'  => 'root',
+          'mode'   => '0644',
+          })
+        should contain_file('access_conf').with_content(
 %{# This file is being maintained by Puppet.
 # DO NOT EDIT
 #
@@ -28,28 +28,29 @@ describe 'pam::accesslogin' do
 # default deny
 - : ALL : ALL
 })
-			end
-		end
-		context 'with multiple users' do
-			let(:facts) do
-				{
-					:osfamily          => 'RedHat',
-					:lsbmajdistrelease => '5',
-				}
-			end
+      end
+    end
+
+    context 'with multiple users on supported platform' do
+      let(:facts) do
+        {
+          :osfamily          => 'RedHat',
+          :lsbmajdistrelease => '5',
+        }
+      end
       let(:pre_condition) do
           'class {"pam": allowed_users => ["foo","bar"] }'
       end
       it do
-      	should include_class('pam')
-				should contain_file('access_conf').with({
-					'ensure' => 'file',
-					'path'   => '/etc/security/access.conf',
-					'owner'  => 'root',
-					'group'  => 'root',
-					'mode'   => '0644',
-					})
-				should contain_file('access_conf').with_content(
+        should include_class('pam')
+        should contain_file('access_conf').with({
+          'ensure' => 'file',
+          'path'   => '/etc/security/access.conf',
+          'owner'  => 'root',
+          'group'  => 'root',
+          'mode'   => '0644',
+          })
+        should contain_file('access_conf').with_content(
 %{# This file is being maintained by Puppet.
 # DO NOT EDIT
 #
@@ -61,7 +62,36 @@ describe 'pam::accesslogin' do
 # default deny
 - : ALL : ALL
 })
-			end
-		end
-	end
+      end
+    end
+
+    context 'with custom values on supported platform' do
+      let(:facts) do
+        {
+          :osfamily          => 'RedHat',
+          :lsbmajdistrelease => '5',
+        }
+      end
+
+      let(:params) do
+        {
+          :access_conf_path     => '/custom/security/access.conf',
+          :access_conf_owner    => 'guido',
+          :access_conf_group    => 'guido',
+          :access_conf_mode     => '0777',
+        }
+      end
+
+      it do
+        should include_class('pam')
+        should contain_file('access_conf').with({
+          'ensure' => 'file',
+          'path'   => '/custom/security/access.conf',
+          'owner'  => 'guido',
+          'group'  => 'guido',
+          'mode'   => '0777',
+          })
+      end
+    end
+  end
 end
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,13 +1,12 @@
-# -*- coding: undecided -*-
 require 'spec_helper'
 describe 'pam' do
 
   describe 'packages' do
 
-    context 'with class defaults on rhel 5' do
+    context 'with class defaults on osfamily redhat with lsbmajdistrelease 5' do
       let :facts do
         {
-          :osfamily          => 'redhat',
+          :osfamily          => 'RedHat',
           :lsbmajdistrelease => '5',
         }
       end
@@ -20,32 +19,90 @@ describe 'pam' do
       end
     end
 
+    context 'with class defaults on osfamily redhat with lsbmajdistrelease 6' do
+      let :facts do
+        {
+          :osfamily          => 'RedHat',
+          :lsbmajdistrelease => '6',
+        }
+      end
+
+      it do
+        should contain_package('pam_package').with({
+          'ensure' => 'installed',
+          'name'   => 'pam',
+        })
+      end
+    end
+
     context 'with specifying package_name on valid platform' do
       let :facts do
         {
-          :osfamily          => 'redhat',
+          :osfamily          => 'RedHat',
           :lsbmajdistrelease => '5',
         }
       end
 
       let(:params) { {:package_name => 'foo'} }
-      
+
       it do
         should contain_package('pam_package').with({
           'ensure' => 'installed',
-      	  'name'   => 'foo',
-				})
+          'name'   => 'foo',
+        })
       end
     end
   end
 
   describe 'config files' do
 
-    context 'defaults for rhel 5' do
+    context 'defaults on osfamily redhat with lsbmajdistrelease 5' do
       let :facts do
         {
-          :osfamily          => 'redhat',
+          :osfamily          => 'RedHat',
           :lsbmajdistrelease => '5',
+        }
+      end
+
+      it do
+        should contain_file('pam_system_auth_ac').with({
+          'ensure'  => 'file',
+          'path'    => '/etc/pam.d/system-auth-ac',
+          'owner'   => 'root',
+          'group'   => 'root',
+          'mode'    => '0644',
+        })
+
+      should contain_file('pam_system_auth').with({
+        'ensure' => 'symlink',
+        'path'   => '/etc/pam.d/system-auth',
+        'owner'  => 'root',
+        'group'  => 'root',
+      })
+
+      should contain_file('pam_d_login').with({
+        'ensure' => 'file',
+        'path'   => '/etc/pam.d/login',
+        'owner'  => 'root',
+        'group'  => 'root',
+        'mode'   => '0644',
+      })
+
+      should contain_file('pam_d_sshd').with({
+        'ensure' => 'file',
+        'path'   => '/etc/pam.d/sshd',
+        'owner'  => 'root',
+        'group'  => 'root',
+        'mode'   => '0644',
+      })
+      end
+    end
+
+    context 'defaults on osfamily redhat with lsbmajdistrelease 6' do
+      let :facts do
+        {
+          :osfamily          => 'RedHat',
+          :lsbmajdistrelease => '6',
         }
       end
 

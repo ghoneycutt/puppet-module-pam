c3bafdafd8004fab264a01e871ba38303e47b508
Garrett Honeycutt <code@garretthoneycutt.com>
2014-02-13 18:34:09 +0100

    Accurately test Solaris. Compress code for other OS's

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -3,10 +3,13 @@ describe 'nsswitch' do
 
   it { should compile.with_all_deps }
 
-  describe 'included class' do
-    context 'with defaults' do
+  describe 'with default values for all parameters' do
+    context 'on osfamily Solaris' do
+      let(:facts) { { :osfamily => 'Solaris' } }
+
+      it { should contain_class('nsswitch') }
+
       it {
-        should contain_class('nsswitch')
         should contain_file('nsswitch_config_file').with({
           'ensure'  => 'file',
           'path'    => '/etc/nsswitch.conf',
@@ -14,6 +17,9 @@ describe 'nsswitch' do
           'group'   => 'root',
           'mode'    => '0644',
         })
+      }
+
+      it {
         should contain_file('nsswitch_config_file').with_content(
 %{# This file is being maintained by Puppet.
 # DO NOT EDIT
@@ -37,25 +43,76 @@ netgroup:   files
 publickey:  files
 automount:  files
 aliases:    files
+ipnodes:    files dns
+printers:   user files
+auth_attr:  files
+prof_attr:  files
+project:    files
 })
       }
     end
 
-    context 'with ldap enabled' do
-      let :params do
-        { :ensure_ldap => 'present' }
-      end
+    ['RedHat','Suse','Debian'].each do |platform|
+      context "on osfamily #{platform}" do
+        let(:facts) { { :osfamily => platform } }
 
-      it {
-        should contain_class('nsswitch')
-        should contain_file('nsswitch_config_file').with({
-          'ensure'  => 'file',
-          'path'    => '/etc/nsswitch.conf',
-          'owner'   => 'root',
-          'group'   => 'root',
-          'mode'    => '0644',
-        })
-        should contain_file('nsswitch_config_file').with_content(
+        it { should contain_class('nsswitch') }
+
+        it {
+          should contain_file('nsswitch_config_file').with({
+            'ensure'  => 'file',
+            'path'    => '/etc/nsswitch.conf',
+            'owner'   => 'root',
+            'group'   => 'root',
+            'mode'    => '0644',
+          })
+        }
+
+        it {
+          should contain_file('nsswitch_config_file').with_content(
+%{# This file is being maintained by Puppet.
+# DO NOT EDIT
+
+passwd:     files
+shadow:     files
+group:      files
+
+sudoers:    files
+
+hosts:      files dns
+
+bootparams: files
+ethers:     files
+netmasks:   files
+networks:   files
+protocols:  files
+rpc:        files
+services:   files
+netgroup:   files
+publickey:  files
+automount:  files
+aliases:    files
+})
+        }
+      end
+    end
+  end
+
+  context 'with ldap enabled' do
+    let :params do
+      { :ensure_ldap => 'present' }
+    end
+
+    it {
+      should contain_class('nsswitch')
+      should contain_file('nsswitch_config_file').with({
+        'ensure'  => 'file',
+        'path'    => '/etc/nsswitch.conf',
+        'owner'   => 'root',
+        'group'   => 'root',
+        'mode'    => '0644',
+      })
+      should contain_file('nsswitch_config_file').with_content(
 %{# This file is being maintained by Puppet.
 # DO NOT EDIT
 
@@ -79,24 +136,24 @@ publickey:  files
 automount:  files ldap
 aliases:    files
 })
-      }
+    }
+  end
+
+  context 'with vas enabled' do
+    let :params do
+      { :ensure_vas => 'present' }
     end
 
-    context 'with vas enabled' do
-      let :params do
-        { :ensure_vas => 'present' }
-      end
-
-      it {
-        should contain_class('nsswitch')
-        should contain_file('nsswitch_config_file').with({
-          'ensure'  => 'file',
-          'path'    => '/etc/nsswitch.conf',
-          'owner'   => 'root',
-          'group'   => 'root',
-          'mode'    => '0644',
-        })
-        should contain_file('nsswitch_config_file').with_content(
+    it {
+      should contain_class('nsswitch')
+      should contain_file('nsswitch_config_file').with({
+        'ensure'  => 'file',
+        'path'    => '/etc/nsswitch.conf',
+        'owner'   => 'root',
+        'group'   => 'root',
+        'mode'    => '0644',
+      })
+      should contain_file('nsswitch_config_file').with_content(
 %{# This file is being maintained by Puppet.
 # DO NOT EDIT
 
@@ -120,126 +177,126 @@ publickey:  files
 automount:  files nis
 aliases:    files
 })
+    }
+  end
+
+  context 'with vas enabled and vas_nss_module_passwd set' do
+    let :params do
+      {
+        :ensure_vas            => 'present',
+        :vas_nss_module_passwd => 'vas3 nis',
       }
     end
 
-    context 'with vas enabled and vas_nss_module_passwd set' do
-      let :params do
-        {
-          :ensure_vas            => 'present',
-          :vas_nss_module_passwd => 'vas3 nis',
-        }
-      end
+    it {
+      should contain_class('nsswitch')
+      should contain_file('nsswitch_config_file').with({
+        'ensure'  => 'file',
+        'path'    => '/etc/nsswitch.conf',
+        'owner'   => 'root',
+        'group'   => 'root',
+        'mode'    => '0644',
+      })
+      should contain_file('nsswitch_config_file').with_content(/passwd:[\s]+files vas3 nis$/)
+    }
+  end
 
-      it {
-        should contain_class('nsswitch')
-        should contain_file('nsswitch_config_file').with({
-          'ensure'  => 'file',
-          'path'    => '/etc/nsswitch.conf',
-          'owner'   => 'root',
-          'group'   => 'root',
-          'mode'    => '0644',
-        })
-        should contain_file('nsswitch_config_file').with_content(/passwd:[\s]+files vas3 nis$/)
+  context 'with vas enabled and vas_nss_module_group set' do
+    let :params do
+      {
+        :ensure_vas           => 'present',
+        :vas_nss_module_group => 'nis',
       }
     end
 
-    context 'with vas enabled and vas_nss_module_group set' do
-      let :params do
-        {
-          :ensure_vas           => 'present',
-          :vas_nss_module_group => 'nis',
-        }
-      end
+    it {
+      should contain_file('nsswitch_config_file').with_content(/group:[\s]+files nis$/)
+    }
+  end
 
-      it {
-        should contain_file('nsswitch_config_file').with_content(/group:[\s]+files nis$/)
+  context 'with vas enabled and vas_nss_module_netgroup set' do
+    let :params do
+      {
+        :ensure_vas              => 'present',
+        :vas_nss_module_netgroup => 'nisplus',
       }
     end
 
-    context 'with vas enabled and vas_nss_module_netgroup set' do
-      let :params do
-        {
-          :ensure_vas              => 'present',
-          :vas_nss_module_netgroup => 'nisplus',
-        }
-      end
+    it {
+      should contain_file('nsswitch_config_file').with_content(/netgroup:[\s]+files nisplus$/)
+    }
+  end
 
-      it {
-        should contain_file('nsswitch_config_file').with_content(/netgroup:[\s]+files nisplus$/)
+  context 'with vas enabled and vas_nss_module_automount set' do
+    let :params do
+      {
+        :ensure_vas               => 'present',
+        :vas_nss_module_automount => 'nisplus',
       }
     end
 
-    context 'with vas enabled and vas_nss_module_automount set' do
-      let :params do
-        {
-          :ensure_vas               => 'present',
-          :vas_nss_module_automount => 'nisplus',
-        }
-      end
+    it {
+      should contain_file('nsswitch_config_file').with_content(/automount:[\s]+files nisplus$/)
+    }
+  end
 
-      it {
-        should contain_file('nsswitch_config_file').with_content(/automount:[\s]+files nisplus$/)
+  context 'with vas enabled and vas_nss_module_automount set empty' do
+    let :params do
+      {
+        :ensure_vas               => 'present',
+        :vas_nss_module_automount => '',
       }
     end
 
-    context 'with vas enabled and vas_nss_module_automount set empty' do
-      let :params do
-        {
-          :ensure_vas               => 'present',
-          :vas_nss_module_automount => '',
-        }
-      end
+    it {
+      should contain_file('nsswitch_config_file').with_content(/automount:[\s]+files$/)
+    }
+  end
 
-      it {
-        should contain_file('nsswitch_config_file').with_content(/automount:[\s]+files$/)
+  context 'with vas enabled and vas_nss_module_aliases set' do
+    let :params do
+      {
+        :ensure_vas             => 'present',
+        :vas_nss_module_aliases => 'nis',
       }
     end
 
-    context 'with vas enabled and vas_nss_module_aliases set' do
-      let :params do
-        {
-          :ensure_vas             => 'present',
-          :vas_nss_module_aliases => 'nis',
-        }
-      end
+    it {
+      should contain_file('nsswitch_config_file').with_content(/aliases:[\s]+files nis$/)
+    }
+  end
 
-      it {
-        should contain_file('nsswitch_config_file').with_content(/aliases:[\s]+files nis$/)
+  context 'with vas enabled and vas_nss_module_services set' do
+    let :params do
+      {
+        :ensure_vas              => 'present',
+        :vas_nss_module_services => 'nis',
       }
     end
 
-    context 'with vas enabled and vas_nss_module_services set' do
-      let :params do
-        {
-          :ensure_vas              => 'present',
-          :vas_nss_module_services => 'nis',
-        }
-      end
+    it {
+      should contain_file('nsswitch_config_file').with_content(/services:[\s]+files nis$/)
+    }
+  end
 
-      it {
-        should contain_file('nsswitch_config_file').with_content(/services:[\s]+files nis$/)
+  context 'with vas and ldap both enabled' do
+    let :params do
+      {
+        :ensure_ldap => 'present',
+        :ensure_vas  => 'present',
       }
     end
 
-    context 'with vas and ldap both enabled' do
-      let :params do
-        {
-          :ensure_ldap => 'present',
-          :ensure_vas  => 'present',
-        }
-      end
-
-      it {
-        should contain_class('nsswitch')
-        should contain_file('nsswitch_config_file').with({
-          'ensure'  => 'file',
-          'path'    => '/etc/nsswitch.conf',
-          'owner'   => 'root',
-          'group'   => 'root',
-          'mode'    => '0644',
-        })
-        should contain_file('nsswitch_config_file').with_content(
+    it {
+      should contain_class('nsswitch')
+      should contain_file('nsswitch_config_file').with({
+        'ensure'  => 'file',
+        'path'    => '/etc/nsswitch.conf',
+        'owner'   => 'root',
+        'group'   => 'root',
+        'mode'    => '0644',
+      })
+      should contain_file('nsswitch_config_file').with_content(
 %{# This file is being maintained by Puppet.
 # DO NOT EDIT
 
@@ -263,100 +320,35 @@ publickey:  files
 automount:  files ldap nis
 aliases:    files
 })
-      }
+    }
+  end
+
+  context 'with config_file set' do
+    let :params do
+      { :config_file => '/path/to/nsswitch.conf' }
     end
 
-    context 'with config_file set' do
-      let :params do
-        { :config_file => '/path/to/nsswitch.conf' }
-      end
+    it {
+      should contain_class('nsswitch')
+      should contain_file('nsswitch_config_file').with({
+        'ensure'  => 'file',
+        'path'    => '/path/to/nsswitch.conf',
+        'owner'   => 'root',
+        'group'   => 'root',
+        'mode'    => '0644',
+      })
+    }
+  end
 
-      it {
+  context 'with config_file set to invalid value' do
+    let :params do
+      { :config_file => 'not/an/absolute/path' }
+    end
+
+    it do
+      expect {
         should contain_class('nsswitch')
-        should contain_file('nsswitch_config_file').with({
-          'ensure'  => 'file',
-          'path'    => '/path/to/nsswitch.conf',
-          'owner'   => 'root',
-          'group'   => 'root',
-          'mode'    => '0644',
-        })
-      }
-    end
-
-    context 'with default options on osfamily Solaris' do
-      let :facts do
-      {
-        :osfamily => 'Solaris',
-      }
-      end
-
-      it {
-        should contain_file('nsswitch_config_file').with_content(/^printers:   user files$/)
-        should contain_file('nsswitch_config_file').with_content(/^ipnodes:    files dns$/)
-        should contain_file('nsswitch_config_file').with_content(/^auth_attr:  files$/)
-        should contain_file('nsswitch_config_file').with_content(/^prof_attr:  files$/)
-        should contain_file('nsswitch_config_file').with_content(/^project:    files$/)
-      }
-    end
-
-    context 'with default options on osfamily RedHat' do
-      let :facts do
-      {
-        :osfamily => 'RedHat',
-      }
-      end
-
-      it {
-        should_not contain_file('nsswitch_config_file').with_content(/^printers:   user files$/)
-        should_not contain_file('nsswitch_config_file').with_content(/^ipnodes:    files dns$/)
-        should_not contain_file('nsswitch_config_file').with_content(/^auth_attr:  files$/)
-        should_not contain_file('nsswitch_config_file').with_content(/^prof_attr:  files$/)
-        should_not contain_file('nsswitch_config_file').with_content(/^project:    files$/)
-      }
-    end
-
-    context 'with default options on osfamily Suse' do
-      let :facts do
-      {
-        :osfamily => 'Suse',
-      }
-      end
-
-      it {
-        should_not contain_file('nsswitch_config_file').with_content(/^printers:   user files$/)
-        should_not contain_file('nsswitch_config_file').with_content(/^ipnodes:    files dns$/)
-        should_not contain_file('nsswitch_config_file').with_content(/^auth_attr:  files$/)
-        should_not contain_file('nsswitch_config_file').with_content(/^prof_attr:  files$/)
-        should_not contain_file('nsswitch_config_file').with_content(/^project:    files$/)
-      }
-    end
-
-    context 'with default options on osfamily Debian' do
-      let :facts do
-      {
-        :osfamily => 'Debian',
-      }
-      end
-
-      it {
-        should_not contain_file('nsswitch_config_file').with_content(/^printers:   user files$/)
-        should_not contain_file('nsswitch_config_file').with_content(/^ipnodes:    files dns$/)
-        should_not contain_file('nsswitch_config_file').with_content(/^auth_attr:  files$/)
-        should_not contain_file('nsswitch_config_file').with_content(/^prof_attr:  files$/)
-        should_not contain_file('nsswitch_config_file').with_content(/^project:    files$/)
-      }
-    end
-
-    context 'with config_file set to invalid value' do
-      let :params do
-        { :config_file => 'not/an/absolute/path' }
-      end
-
-      it do
-        expect {
-          should contain_class('nsswitch')
-        }.to raise_error(Puppet::Error)
-      end
+      }.to raise_error(Puppet::Error)
     end
   end
 end

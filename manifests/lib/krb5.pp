12dd72f8c151eec52e462e851ccb683e0ed3503e
Aaron Hicks <hicksa@landcareresearch.co.nz>
2015-02-09 16:50:27 +1300

    add classes to install some common PAM lib module things

---
+++ manifests/lib/krb5.pp
@@ -0,0 +1,34 @@
+class pam::lib::krb5 (
+  $ensure  = 'installed',
+  $package = undef
+) {
+
+  include pam
+
+  if $package {
+    $my_package = $package
+  } else {
+    case $::osfamily {
+    'RedHat': {
+      $my_package = 'pam_krb5'
+    }
+    'Debian': {
+      $my_package = 'libpam-krb5'
+    }
+    'Suse':{
+      $my_package = 'pam_krb5'
+    }
+    # 'Solaris':{
+    #   # No packages for Solaris
+    # }
+    default:{
+      fail("a custom PAM Kerberos5 module package is required for ${::osfamily}")
+    }
+  }
+  }
+
+  package{$my_package:
+    ensure => $ensure,
+  }
+
+}

212c5c393d767ddbbd7e34e51aea3098dad12dd5
Garrett Honeycutt <code@garretthoneycutt.com>
2015-02-18 14:17:28 +0100

    Work with Aethylred on spec tests

--- spec/defines/service_spec.rb
+++ spec/defines/service_spec.rb
@@ -7,51 +7,56 @@ describe 'pam::service', :type => :define do
         :operatingsystemmajrelease  => '5',
       }
     }
+
     context 'with no parameters' do
       let(:title) { 'test' }
+
       it { should contain_class('pam') }
-      it { should contain_file('pam.d-service-test').with(
-        'ensure'  => 'file',
-        'owner'   => 'root',
-        'group'   => 'root',
-        'mode'    => '0644',
-        'content' => nil
-      ) }
+
+      it {
+        should contain_file('pam.d-service-test').with({
+          'ensure'  => 'file',
+          'owner'   => 'root',
+          'group'   => 'root',
+          'mode'    => '0644',
+          'content' => nil,
+        })
+      }
     end
+
     context 'when absent' do
       let(:title) { 'test' }
-      let :params do
-          {
-            :ensure  => 'absent'
-          }
-        end
-      it { should contain_file('pam.d-service-test').with(
-        'ensure'  => 'absent'
-      ) }
+      let(:params) { { :ensure => 'absent' } }
+
+      it {
+        should contain_file('pam.d-service-test').with({
+          'ensure'  => 'absent',
+        })
+      }
     end
+
     context 'when given content' do
       let(:title) { 'test' }
-      let :params do
-          {
-            :content  => 'session required pam_permit.so'
-          }
-        end
+      let(:params) { { :content => 'session required pam_permit.so' } }
+
       it { should contain_file('pam.d-service-test').with_content(
-        'session required pam_permit.so'
+        %{session required pam_permit.so}
       ) }
     end
+
     context 'when given an array of lines' do
       let(:title) { 'test' }
-      let :params do
-          {
-            :lines  => [
-              '@include common-auth',
-              '@include common-account',
-              'session required pam_permit.so',
-              'session required pam_limits.so'
-            ]
-          }
-        end
+      let(:params) do
+        {
+          :lines  => [
+            '@include common-auth',
+            '@include common-account',
+            'session required pam_permit.so',
+            'session required pam_limits.so',
+          ],
+        }
+      end
+
       it { should contain_file('pam.d-service-test').with_content(
         %{# This file is being maintained by Puppet.
 # DO NOT EDIT

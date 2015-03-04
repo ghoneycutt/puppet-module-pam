e8d700e7afda3f5e4b016c85d6226ebb69ff7772
Garrett Honeycutt <code@garretthoneycutt.com>
2015-03-04 15:58:36 -0500

    Rework pam::lib::ccreds
    example for rest of pam::lib::* classes

--- manifests/lib/ccreds.pp
+++ manifests/lib/ccreds.pp
@@ -1,67 +1,71 @@
+# == Class: pam::lib::ccreds
+#
 class pam::lib::ccreds (
-  $ensure  = 'installed',
-  $package = undef
+  $package_ensure = 'installed',
+  $package_name   = 'USE_DEFAULTS',
 ) {
 
-  if $package {
-    $my_package = $package
-  } else {
-    case $::osfamily {
-      'RedHat': {
-        case $::operatingsystemmajrelease {
-          '5','6','7': {
-            $my_package = 'pam_ccreds'
-          }
-          default: {
-            fail("a custom PAM ccreds module package is required for ${::osfamily} release ${::operatingsystemmajrelease}")
-          }
+  case $::osfamily {
+    'RedHat': {
+      case $::operatingsystemmajrelease {
+        '5','6','7': {
+          $default_package_name = 'pam_ccreds'
+        }
+        default: {
+          fail("pam::lib::ccreds supports EL 5, 6 and 7. Your operatingsystemmajrelease is identified as <${::operatingsystemmajrelease}>.")
         }
       }
-      'Debian': {
-        case $::lsbdistid{
-          'Ubuntu': {
-            case $::lsbdistrelease {
-              '12.04','14.04': {
-                $my_package = 'libpam-ccreds'
-              }
-              default: {
-                fail("a custom PAM ccreds module package is required for ${::osfamily} release ${::operatingsystemmajrelease}")
-              }
+    }
+    'Debian': {
+      case $::lsbdistid {
+        'Ubuntu': {
+          case $::lsbdistrelease {
+            '12.04','14.04': {
+              $default_package_name = 'libpam-ccreds'
+            }
+            default: {
+              fail("pam::lib::ccreds supports Ubuntu 12.04 and 14.04. Your lsbdistrelease is identified as <${::lsbdistrelease}>.")
             }
           }
-          default:{
-            case $::operatingsystemmajrelease {
-              '6': {
-                $my_package = 'libpam-ccreds'
-              }
-              default: {
-                fail("a custom PAM ccreds module package is required for ${::osfamily} release ${::operatingsystemmajrelease}")
-              }
+        }
+        default:{
+          case $::operatingsystemmajrelease {
+            '6': {
+              $default_package_name = 'libpam-ccreds'
+            }
+            default: {
+              fail("pam::lib::ccreds supports Debian 6. Your operatingsystemmajrelease is identified as <${::operatingsystemmajrelease}>.")
             }
           }
         }
       }
-      'Suse':{
-        case $::lsbmajdistrelease {
-          '9','10','11','12': {
-            $my_package = 'pam_ccreds'
-          }
-          default: {
-            fail("a custom PAM ccreds module package is required for ${::osfamily} release ${::operatingsystemmajrelease}")
-          }
+    }
+    'Suse': {
+      case $::lsbmajdistrelease {
+        '9','10','11','12': {
+          $default_package_name = 'pam_ccreds'
+        }
+        default: {
+          fail("pam::lib::ccreds supports Suse 10, 11, and 12. Your lsbmajdistrelease is identified as <${::lsbmajdistrelease}>.")
         }
       }
-      # 'Solaris':{
-      #   # No packages for Solaris
-      # }
-      default:{
-        fail("a custom PAM ccreds module package is required for ${::osfamily}")
-      }
+    }
+    default: {
+      fail('pam::lib::ccreds does not have a default for your system. Please specify a package at pam::lib::ccreds::package.')
     }
   }
 
-  package{$my_package:
-    ensure => $ensure,
+  if $package_name == 'USE_DEFAULTS' {
+    $package_name_real = $default_package_name
+  } else {
+    $package_name_real = $package_name
   }
 
+  validate_string($package_name_real)
+  validate_string($package_ensure)
+
+  package { 'pam_lib_ccreds':
+    ensure => $package_ensure,
+    name   => $package_name_real,
+  }
 }

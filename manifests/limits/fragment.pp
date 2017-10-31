aef62337810b78b0145f192106e1b1705147a603
Trey Dockendorf <tdockendorf@osc.edu>
2017-10-31 12:27:52 -0400

    Fix conditions to not use == undef and != undef

--- manifests/limits/fragment.pp
+++ manifests/limits/fragment.pp
@@ -17,22 +17,18 @@ define pam::limits::fragment (
   }
 
   # must specify source or list
-  if $ensure != 'absent' and $source == 'UNSET' and $list == undef {
+  if $ensure != 'absent' and $source == 'UNSET' and ! $list {
     fail('pam::limits::fragment must specify source or list.')
   }
 
   # list takes priority if you specify both
-  if $list == undef {
-    $source_real = $source
-  } else {
-    $source_real = undef
-  }
-
   # use the template if a list is provided
-  if $list == undef {
-    $content = undef
-  } else {
+  if $list {
+    $source_real = undef
     $content = template('pam/limits_fragment.erb')
+  } else {
+    $source_real = $source
+    $content = undef
   }
 
   file { "${pam::limits::limits_d_dir}/${name}.conf":

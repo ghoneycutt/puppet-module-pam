d2262db89ba51a8c6254e6db1d7dd8a908255e89
Phil Friderici <phil.friderici@i-tee.de>
2018-01-07 16:57:17 +0000

    Parameterize pam::accesslogin::allowed_users

--- manifests/accesslogin.pp
+++ manifests/accesslogin.pp
@@ -1,21 +1,41 @@
-# == Class: pam::accesslogin
+# @summary Manage login access
+#   See PAM_ACCESS(8)
 #
-# Manage login access
+# @example Declaring the class
+#   include ::pam::accesslogin
 #
-# See PAM_ACCESS(8)
+# @param access_conf_path
+#   Path to access.conf.
+#
+# @param access_conf_owner
+#   Owner of access.conf.
+#
+# @param access_conf_group
+#   Group of access.conf.
+#
+# @param access_conf_mode
+#   Mode of access.conf.
+#
+# @param access_conf_template
+#   Content template of access.conf.
+#
+# @param allowed_users
+#   String, Array or Hash of strings and/or arrays to configure users and
+#   origins in access.conf. The default allows the root user/group from
+#   origin 'ALL'.
 #
 class pam::accesslogin (
-  Stdlib::Absolutepath $access_conf_path  = '/etc/security/access.conf',
-  String $access_conf_owner               = 'root',
-  String $access_conf_group               = 'root',
-  Stdlib::Filemode $access_conf_mode      = '0644',
-  String $access_conf_template            = 'pam/access.conf.erb',
-) {
+  Stdlib::Absolutepath $access_conf_path      = '/etc/security/access.conf',
+  String $access_conf_owner                   = 'root',
+  String $access_conf_group                   = 'root',
+  Stdlib::Filemode $access_conf_mode          = '0644',
+  String $access_conf_template                = 'pam/access.conf.erb',
+  Variant[Array, Hash, String] $allowed_users = $pam::allowed_users,
+) inherits ::pam {
 
-  require '::pam'
-
-  $allowed_users = $::pam::allowed_users
-
+  # transform $allowed_users into a valid hash
+  # origin defaults to 'ALL' if unset
+  # if origin is an array, create a space separated list
   case $allowed_users {
     String: {
       $allowed_users_hash = { $allowed_users => 'ALL' }

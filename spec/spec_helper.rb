d96439ce9498dc6981cd491c8b00660faaf48049
Garrett Honeycutt <code@garretthoneycutt.com>
2018-04-30 10:42:44 -0400

    Modify spec helper to remove warning
    See https://tickets.puppetlabs.com/browse/PDK-916

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,3 +1,8 @@
+# This RSpec.configure block is listed twice due to a bug with
+# puppetlabs_spec_helper. https://tickets.puppetlabs.com/browse/PDK-916
+RSpec.configure do |config|
+  config.mock_with :rspec
+end
 require 'puppetlabs_spec_helper/module_spec_helper'
 
 RSpec.configure do |config|
